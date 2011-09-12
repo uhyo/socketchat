184f1b2c376eae46d20ac2695f3b5a4fbed3d6aa
uhyo <no@ubuntu-vbox.(none)>
2011-09-12 23:26:49 +0900

    shabetter

--- ch.js
+++ ch.js
@@ -4,8 +4,8 @@ var socketio=require('socket.io'),mongodb=require('mongodb');
 //定数定義
 DB_PORT=27017;
 DB_NAME="socketchat";
-CHAT_FIRST_LOG=10;	//最初どれだけログ表示するか
-CHAT_MOTTO_LOG=10;	//HottoMotto時にログをどれだけ表示するか
+CHAT_FIRST_LOG=100;	//最初どれだけログ表示するか
+CHAT_MOTTO_LOG=100;	//HottoMotto時にログをどれだけ表示するか
 
 CHAT_NAME_MAX_LENGTH = 25;
 CHAT_MAX_LENGTH = 1000;
@@ -91,6 +91,60 @@ db.open(function(err,_db){
 });
 var users=[],users_next=1;
 
+var filters=[];
+
+//拡張
+//ｼｬﾍﾞｯﾀｰ
+filters.push(function(logobj){
+	var date=new Date(logobj.time);
+	var minutes=date.getMinutes();
+	if(minutes==0 || minutes==30){
+		//半角カナに変換
+		var table={
+"ぁ":"ｧ", "あ":"ｱ", "ぃ":"ｨ", "い":"ｲ", "ぅ":"ｩ", "う":"ｳ", "ぇ":"ｪ", "え":"ｴ", "ぉ":"ｫ", "お":"ｵ",
+"か":"ｶ","が":"ｶﾞ", "き":"ｷ","ぎ":"ｷﾞ", "く":"ｸ","ぐ":"ｸﾞ", "け":"ｹ","げ":"ｹﾞ", "こ":"ｺ","ご":"ｺﾞ",
+"さ":"ｻ","ざ":"ｻﾞ", "し":"ｼ","じ":"ｼﾞ", "す":"ｽ","ず":"ｽﾞ", "せ":"ｾ","ぜ":"ｾﾞ", "そ":"ｿ","ぞ":"ｿﾞ",
+"た":"ﾀ","だ":"ﾀﾞ", "ち":"ﾁ","ぢ":"ﾁﾞ", "っ":"ｯ", "つ":"ﾂ","づ":"ﾂﾞ", "て":"ﾃ","で":"ﾃﾞ", "と":"ﾄ","ど":"ﾄﾞ",
+"な":"ﾅ", "に":"ﾆ", "ぬ":"ﾇ", "ね":"ﾈ", "の":"ﾉ",
+"は":"ﾊ","ば":"ﾊﾞ","ぱ":"ﾊﾟ", "ひ":"ﾋ","び":"ﾋﾞ","ぴ":"ﾋﾟ", "ふ":"ﾌ","ぶ":"ﾌﾞ","ぷ":"ﾌﾟ", "へ":"ﾍ","べ":"ﾍﾞ","ぺ":"ﾍﾟ", "ほ":"ﾎ","ぼ":"ﾎﾞ","ぽ":"ﾎﾟ",
+"ま":"ﾏ", "み":"ﾐ", "む":"ﾑ", "め":"ﾒ", "も":"ﾓ",
+"ゃ":"ｬ", "や":"ﾔ", "ゅ":"ｭ", "ゆ":"ﾕ", "ょ":"ｮ", "よ":"ﾖ",
+"ら":"ﾗ", "り":"ﾘ", "る":"ﾙ", "れ":"ﾚ", "ろ":"ﾛ", "わ":"ﾜ", "を":"ｦ", "ん":"ﾝ",
+"ヴ":"ｳﾞ","ヷ":"ﾜﾞ", "ヰ":"ｲ", "ヸ":"ｲﾞ","ヹ":"ｴﾞ","ヺ":"ｦﾞ","ー":"ｰ", "ゕ":"ｶ","ゖ":"ｹ",
+"゛":"ﾞ", "゜":"ﾟ",
+		};
+		var bgrs=["ｧｱｶｻﾀﾅﾊﾏﾔｬﾗﾜ","ｨｲｷｼﾁﾆﾋﾐﾘ","ｩｳｸｽﾂﾇﾌﾑﾕｭﾙ","ｪｴｹｾﾃﾈﾍﾒﾚ","ｫｵｺｿﾄﾉﾎﾓﾖｮﾛｦ"];
+		var boin=["ｧ","ｨ","ｩ","ｪ","ｫ"];
+
+		var comment=logobj.comment.replace(/[ァ-ヶ]/g,function(katakana){
+			return String.fromCharCode(hiragana.charCodeAt(0)+(0x3040-0x30A0));
+		}).replace(/[ぁ-ゖ゛゜ヷ-ヺ]/g,function(hiragana){
+			return table[hiragana] || hiragana;
+		}).replace(/([ｦ-ﾟ]+)/g,function(katakanas){
+			var ato="",last=null;
+			var pt=katakanas.length;
+			while(pt){
+				last=katakanas[--pt];
+				if(last=="ｰ" || last=="ﾞ" || last=="ﾟ"){
+					continue;
+				}
+				break;
+			}
+			for(var i=0;i<5;i++){
+				if(bgrs[i].indexOf(last)>=0){
+					//母音
+					for(var j=0;j<5;j++)katakanas+=boin[i];
+					break;
+				}
+			}
+			return katakanas;
+		});
+		logobj.comment=comment;
+		
+	}
+});
+
+
 httpserver.listen(8080);
 
 var io=socketio.listen(httpserver);
@@ -172,6 +226,9 @@ function says(socket,user,data){
 	makelog(socket,logobj);
 }
 function makelog(socket,logobj){
+	filters.forEach(function(func){
+		func(logobj);
+	});
 	log.insert(logobj,{"safe":true},function(err,docs){
 		if(err){
 			console.log(err);
