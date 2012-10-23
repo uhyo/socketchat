c0ccf8d7cd27054d456f4d1f81beaf9a3bdc99b6
Mamesoft <info@mamesoft.jp>
2012-10-23 22:57:11 +0900

    SmartPhone UI Bata

--- ch.js
+++ ch.js
@@ -46,10 +46,10 @@ var app=require('express')();
 app.get(/^\/(index\.html)?$/, function(req, res){
 	res.sendfile(__dirname + '/index.html');
 });
-app.get(/^\/(log|list|apiclient|com)(\.js)?$/, function(req, res){
+app.get(/^\/(log|list|apiclient|com|smp)(\.js)?$/, function(req, res){
 	res.sendfile(__dirname + "/"+req.params[0]+'.'+(req.params[1]?'js':'html'));
 });
-app.get(/^\/((?:line|connection|client)\.js|css\.css|(sound|jihou)\.(mp3|wav|ogg))$/, function(req, res){
+app.get(/^\/((?:line|connection|client)\.js|css\.css|(sound|jihou)\.(mp3|wav|ogg)|smp.css)$/, function(req, res){
 	res.sendfile(__dirname + "/"+req.params[0]);
 });
 
